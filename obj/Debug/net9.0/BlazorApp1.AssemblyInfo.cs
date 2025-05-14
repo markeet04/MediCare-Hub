9df9f692b1dca9544c5800a4732501f2cd3d88ed
bilaladnan05 <m.bilal.adnan05@gmail.com>
2025-05-14 09:19:35 +0500

    receptionist view v1

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdc9e5f2f59343af4c5dd2a54ea01df09f6aa08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0556c2775f706b830a22a2894df6a7e99bd49ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
